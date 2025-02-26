61938dc15bdbe03a054d80d636ccfc80c8983f64
LukaStuber <pajo024@gmail.com>
2025-02-26 13:07:30 +0100

    change snake to int array instead of bool. made a method to check if the head is on an apple. not in use yet

--- Snake.cs
+++ Snake.cs
@@ -1,59 +1,75 @@
-﻿namespace SnakeRedux {
-    class Snake {        
-        struct Board {
+﻿namespace SnakeRedux
+{
+    class Snake
+    {
+        struct Board
+        {
             public int width;
             public int height;
             public int[,] board;
-            public Board(int x, int y) {
+            public Board(int x, int y)
+            {
                 width = x;
                 height = y;
                 board = new int[width, height];
             }
         }
-        struct Player {
+        struct Player
+        {
             public int x;
             public int y;
             public int length;
-            public bool[,] snake;
-            public Player(Board board) {
+            public int[,] snake;
+            public Player(Board board)
+            {
                 x = board.width / 2;
                 y = board.height / 2;
                 length = 1;
-                snake = new bool[board.width, board.height];
-                snake[x, y] = true;
+                snake = new int[board.width, board.height];
+                snake[x, y] = 1;
             }
         }
-        struct Apple {
+        struct Apple
+        {
             public int count;
             public int[] x;
             public int[] y;
             public bool[,] apples;
-            public Apple(int num, Board board) {
+            public Apple(int num, Board board)
+            {
                 Random r = new Random();
                 count = num;
                 x = new int[count];
                 y = new int[count];
                 apples = new bool[board.width, board.height];
-                for (int i = 0; i < count; i++) {
+                for (int i = 0; i < count; i++)
+                {
                     x[i] = r.Next(0, board.width);
                     x[i] = r.Next(0, board.height);
                     apples[x[i], y[i]] = true;
                 }
             }
         }
-        static void Main() {
+        static void Main()
+        {
             Console.Clear();
             Console.Title = "slayworldsnake";
             Console.CursorVisible = false;
-            
+
             ConsoleKey lastInput;
+            ConsoleKey extraInput;
+            extraInput = ConsoleKey.None;
+
             Board board = new Board(10, 9);
             Player player = new Player(board);
+
             Console.WriteLine("Enter to start...");
             lastInput = Console.ReadKey().Key;
-            
-            while (lastInput != ConsoleKey.Enter) {
-                if (lastInput == ConsoleKey.Escape) {
+
+            while (lastInput != ConsoleKey.Enter)
+            {
+                if (lastInput == ConsoleKey.Escape)
+                {
                     Environment.Exit(0);
                 }
                 lastInput = Console.ReadKey(true).Key;
@@ -61,23 +77,28 @@
             lastInput = ConsoleKey.D;
             Thread inputThread = new Thread(delegate ()
             {
-                InputLoop(ref lastInput);
+                InputLoop(ref lastInput, ref extraInput);
             });
             inputThread.Start();
-            GameLoop(ref lastInput, board, player);
+            GameLoop(board, player, ref lastInput, ref extraInput);
         }
-        static void GameLoop(ref ConsoleKey lastInput, Board board, Player player) {
+        static void GameLoop(Board board, Player player, ref ConsoleKey lastInput, ref ConsoleKey extraInput)
+        {
             bool gameOver = false;
             bool gameWon = false;
-            while (true) {
-                if (player.x > board.width || player.x < 0 || player.y > board.height || player.y < 0) {
+            while (true)
+            {
+                if (player.x > board.width || player.x < 0 || player.y > board.height || player.y < 0)
+                {
                     gameOver = true;
                 }
-                if (player.length == board.width * board.height) {
+                if (player.length == board.width * board.height)
+                {
                     gameWon = true;
                 }
                 Render(board, player);
-                switch (lastInput) {
+                switch (extraInput)
+                {
                     case ConsoleKey.Escape:
                         EndGame();
                         break;
@@ -85,6 +106,10 @@
                         RestartGame(board, ref player, ref lastInput);
                         gameOver = false;
                         break;
+                }
+                extraInput = ConsoleKey.None;
+                switch (lastInput)
+                {
                     case ConsoleKey.W:
                         player.y -= 1;
                         break;
@@ -98,31 +123,35 @@
                         player.x += 1;
                         break;
                 }
-                if (gameOver) {
+                if (gameOver)
+                {
                     Console.Clear();
                     Console.WriteLine("You lose!");
                     Console.WriteLine("Press any key to restart...");
                     Console.WriteLine("Press Escape to exit...");
-                    switch (lastInput) {
+                    switch (lastInput)
+                    {
                         case ConsoleKey.Escape:
                             EndGame();
                             break;
-                        case ConsoleKey.Enter:
+                        default:
                             RestartGame(board, ref player, ref lastInput);
                             gameOver = false;
                             break;
                     }
                 }
-                if (gameWon) {
+                if (gameWon)
+                {
                     Console.Clear();
                     Console.WriteLine("You Win!");
                     Console.WriteLine("Press any key to restart...");
                     Console.WriteLine("Press Escape to exit...");
-                    switch (lastInput) {
+                    switch (lastInput)
+                    {
                         case ConsoleKey.Escape:
                             EndGame();
                             break;
-                        case ConsoleKey.Enter:
+                        default:
                             RestartGame(board, ref player, ref lastInput);
                             gameWon = false;
                             break;
@@ -131,25 +160,59 @@
                 Thread.Sleep(500);
             }
         }
-        static void InputLoop(ref ConsoleKey lastInput) {
-            while (true) {
-                lastInput = Console.ReadKey(true).Key;
+        static void InputLoop(ref ConsoleKey lastInput, ref ConsoleKey extraInput)
+        {
+            ConsoleKey input;
+            while (true)
+            {
+                input = Console.ReadKey(true).Key;
+                if (input == ConsoleKey.W || input == ConsoleKey.A || input == ConsoleKey.S || input == ConsoleKey.D)
+                {
+                    lastInput = input;
+                }
+                else
+                {
+                    extraInput = input;
+                }
             }
         }
-        static void RestartGame(Board board, ref Player player, ref ConsoleKey lastInput) {
+        static void RestartGame(Board board, ref Player player, ref ConsoleKey lastInput)
+        {
             player = new Player(board);
             lastInput = ConsoleKey.D;
         }
-        static void EndGame() {
+        static void EndGame()
+        {
             Console.Clear();
             Console.CursorVisible = true;
             Environment.Exit(0);
         }
-        static void Render(Board board, Player player) {
+
+        static void OnApple(Board board, ref Apple apple, ref Player player)
+        {
+            Random r = new();
+            int x, y;
+
+            if (apple.apples[player.x, player.y] == true)
+            {
+                player.length++;
+                apple.apples[player.x, player.y] = false;
+
+                x = r.Next(0, board.width);
+                x = r.Next(0, board.height);
+                apple.apples[x, y] = true;
+            }
+        }
+
+        static void Render(Board board, Player player)
+        {
             Console.Clear();
-            for (int y = 0; y < board.height; y++) {
-                for (int x = 0; x < board.width; x++) {
-                    if (player.x == x && player.y == y) {
+            for (int y = 0; y < board.height; y++)
+            {
+                for (int x = 0; x < board.width; x++)
+                {
+                    if (player.x == x && player.y == y)
+                    {
                         Console.BackgroundColor = ConsoleColor.Blue;
                     }
                     else if (x % 2 == 0 && y % 2 == 0)
@@ -167,8 +230,10 @@
                     Console.Write("    ");
                 }
                 Console.WriteLine();
-                for (int x = 0; x < board.width; x++) {
-                    if (player.x == x && player.y == y) {
+                for (int x = 0; x < board.width; x++)
+                {
+                    if (player.x == x && player.y == y)
+                    {
                         Console.BackgroundColor = ConsoleColor.Blue;
                     }
                     else if (x % 2 == 0 && y % 2 == 0)
